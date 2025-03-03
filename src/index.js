1b1390ec63bc332e93860098c7ecce1668965196
Your Name <you@example.com>
2025-03-03 20:16:07 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js');
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/style.css';
+import '@fortawesome/fontawesome-free/js/all.min.js';
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+console.log ("اهلا بك في متجر عربي");
+console.log ("اهلا بك في اكاديمية حسوب");

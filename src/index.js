b99b9c16343137e0a0aca224e4df6aa758af59f4
Your Name <you@example.com>
2025-03-10 15:14:23 +0200

    brand products done

--- src/index.js
+++ src/index.js
@@ -5,5 +5,11 @@ import '@fortawesome/fontawesome-free/js/all.min.js';
 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+document.querySelectorAll('.add-to-cart-btn').forEach(item => {
+    item.addEventListener("click" , () =>{
+        alert("أضيف المنتج إلى عربة الشراء")
+    })
+})
 console.log ("اهلا بك في متجر عربي");
 console.log ("اهلا بك في اكاديمية حسوب");
